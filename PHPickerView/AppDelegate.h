34979c72162cf47c44b342f9e201d8a9a0693866
Andrzej <andrewfrankowski@gmail.com>
2015-12-03 16:49:49 +0100

    Initial Commit

---
+++ PHPickerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PHPickerView
+//
+//  Created by Andrzej on 03/12/15.
+//  Copyright © 2015 A&A.make LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PHPickerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PHPickerView
+//
+//  Created by Andrzej on 03/12/15.
+//  Copyright © 2015 A&A.make LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
